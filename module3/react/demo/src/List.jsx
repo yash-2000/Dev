c8085ab6ba8eb2114afda930493fff45ac43be5b
yash <yashchaudhary7665@gmail.com>
2021-08-09 12:35:48 +0530

    up

---
+++ module3/react/demo/src/List.jsx
@@ -0,0 +1,22 @@
+let List = (props) => {
+    return (
+      <ul>
+        {props.tasks.map((el) => {
+          return (
+            <li>
+              {el}
+              <button
+                onClick={() => {
+                  props.deleteTask(el)
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
+  
+  export default List;
